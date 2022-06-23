563c7d95f90a2836546e8bc21c047741136023bb
Elvis <elvisgiltorres@gmail.com>
2022-06-23 12:39:08 -0400

    Mejorando el menu del header para moverse por las rutas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,43 @@ import { useSelector } from 'react-redux';
 import {active} from '../Slices/menuSlice';
 import { useDispatch } from 'react-redux';
 
+
+const links_menu = [
+  {
+      id:1,
+      name:"Inicio",
+      path:"#",
+      src_img:"https://i.ibb.co/XYm7dCD/icons8-casa-24.png",
+      alt_img:"icono de casa",
+
+  },
+  {
+      id:2,
+      name:"Explorar",
+      path:"#",
+      src_img:"https://i.ibb.co/8j5fgSC/icons8-globo-terr-queo-24.png",
+      alt_img:"icono de globo terraqueo",
+
+  },
+  {
+      id:3,
+      name:"Favoritos",
+      path:"#",
+      src_img:"https://i.ibb.co/pnvGndr/icons8-agregar-a-favoritos-24.png",
+      alt_img:"icono de favoritos",
+
+  },
+  {
+    id:4,
+    name:"Acerca",
+    path:"#",
+    src_img:"https://i.ibb.co/VqPK7nL/icons8-cuadrado-de-informaci-n-24.png",
+    alt_img:"icono de informacion",
+
+},
+
+];
+
 export const Header = () => {
   const menu = useSelector(state => state.menu.value);
   const dispatch = useDispatch();
@@ -26,11 +63,15 @@ export const Header = () => {
               <span className='background-dark'></span>
               <nav className='Header-menu'>
                 <img src="https://i.ibb.co/HCmN2MN/icons8-eliminar-50.png" alt="icono de cerrar" onClick={handleClose} className='Header-menu-close'/>
-                  <a href='#' className='Header-menu-link'><img className="menu-icon-link" src='https://i.ibb.co/XYm7dCD/icons8-casa-24.png' alt='icono de casa'/>Inicio</a>
-                  <a href='#' className='Header-menu-link'><img className="menu-icon-link" src='https://i.ibb.co/8j5fgSC/icons8-globo-terr-queo-24.png' alt='icono de mundo' />Explorar</a>    
-                  <a href='#' className='Header-menu-link'><img className="menu-icon-link" src='https://i.ibb.co/pnvGndr/icons8-agregar-a-favoritos-24.png' alt='icono de favoritos'/>Favoritos</a>    
-                  <a href='#' className='Header-menu-link'><img className="menu-icon-link" src='https://i.ibb.co/VqPK7nL/icons8-cuadrado-de-informaci-n-24.png' alt='icono de informacion'/>Acerca</a> 
-              </nav>
+                  {
+                    links_menu.map(item => (
+                      <a key={item.id} href={item.path} className='Header-menu-link'>
+                        <img className="menu-icon-link" src={item.src_img} alt={item.alt_img}/>
+                        {item.name}
+                      </a> 
+                    ))
+                  }
+                </nav>
             </>
       }
     </div>
